a78798cadd3f745880178fab96e8efd984c7b69a
il94 <ilyes.landolsi@outlook.fr>
2025-12-03 02:27:30 +0100

    chore: update default port to 3000 in bootstrap function

--- back/src/main.ts
+++ back/src/main.ts
@@ -40,6 +40,6 @@ async function bootstrap() {
 	app.use(passport.initialize());
 	app.use(passport.session());
 		
-	await app.listen(process.env.PORT || 3333);
+	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
